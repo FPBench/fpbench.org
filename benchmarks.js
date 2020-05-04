41f6f9df78afb3271e601d822de355991d5c8630
Pavel Panchekha <me@pavpanchekha.com>
2020-05-04 17:49:27 -0600

    New logo + website

--- benchmarks.js
+++ benchmarks.js
@@ -143,9 +143,9 @@ function create_fptaylor_permalink(core) {
 }
 
 function render_result(core) {
-    var more_link = Element("a", { className: "more" }, "more");
+    var more_link = Element("a", { className: "more", href: "" }, "more");
     var out = Element("div", [
-        Element("h2", [ core[":name"] || "(unnamed)", more_link ]),
+        Element("h3", [ core[":name"] || "(unnamed)", more_link ]),
         core[":description"] && render_datum("Description", "p", core[":description"]),
         core[":example"] && render_datum("Example", "span", render_example(core[":example"])),
 
@@ -181,8 +181,10 @@ function render_result(core) {
     out.addEventListener("click", function() { out.classList.add("open"); more_link.textContent = "less" });
     more_link.addEventListener("click", function(e) {
         out.classList.toggle("open");
+        e.preventDefault();
         e.stopPropagation();
         more_link.textContent = out.classList.contains("open") ? "less" : "more";
+        return false;
     });
     return out;
 }
